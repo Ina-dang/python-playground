9ca7caec0e9d5efa3dfa137835ba61c9c4bd85de
Ina <ina9377@gmail.com>
2026-02-17 21:01:23 +0900

    입력 대화창 구성

--- ai-study/chatbot.py
+++ ai-study/chatbot.py
@@ -14,6 +14,14 @@ def main():
         openai_api_key = st.text_input("OpenAI API key", type="password")
         st.write("[OpenAI API Key 받기](https://platform.openai.com/account/api-keys)")
 
+    # 입력 대화창 구성
+    user_input = st.chat_input("무엇이 궁금한가요?")
+    if user_input:
+        with st.chat_message("user"):
+            st.write(user_input)
+        with st.chat_message("assistant"):
+            st.write("안녕! 난 친구봇이야. :D")
+
 
 # main 함수 실행
 if __name__ == "__main__":
