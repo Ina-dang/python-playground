3d27fe0327b4e9d759a9e9e200c5f483ad599357
Ina <ina9377@gmail.com>
2026-02-17 20:53:09 +0900

    message_history 추가

--- ai-study/test_openai.py
+++ ai-study/test_openai.py
@@ -4,18 +4,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY"),
-)
+client = OpenAI()
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Say this is a test",
-        }
-    ],
-    model="gpt-4o",
-)
+# 사용자&AI 대화 내역 저장
+message_history = []
 
-print(chat_completion.choices[0].message.content)
+while True:
+
+    # 사용자 입력
+    user_input = input("사용자: ")
+    message_history.append({"role": "user", "content": user_input})
+    chat_completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=message_history,  # 이전 대화 내역 모두 저장
+    )
+
+    # AI 응답
+    assistant_response = chat_completion.choices[0].message.content
+    message_history.append({"role": "assistant", "content": assistant_response})
+
+    print(f"챗봇: {assistant_response}")
