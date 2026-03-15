fd7363b5ae3301b93ba8ef818d16a4242b4fde5b
Ina <ina9377@gmail.com>
2026-03-15 18:18:29 +0900

    데이터 프레임 변환 및 다운로드 추가

--- ai-study/analysis_img.py
+++ ai-study/analysis_img.py
@@ -2,6 +2,9 @@ from openai import OpenAI
 import base64
 import streamlit as st
 
+import pandas as pd
+import re
+
 
 # 이미지 인코딩 함수 정의
 def encode_image(image_path):
@@ -32,6 +35,37 @@ def analyze_image(prompt, base64_image, client):
         ],
     )
     return response.choices[0].message.content
+
+
+# 마크다운 - 데이터프레임 변환 함수 정의
+def markdown_to_dataframes(markdown_text):
+    # 마크다운 표를 찾기 위한 정규 표현식 패턴
+    table_pattern = r"\|(.+)\|\n\|(-+\|)+\n(((\|.+\|\n)+\|.+\|)\n?)"
+    tables = re.findall(table_pattern, markdown_text)
+    dataframes = []
+
+    for table in tables:
+        header = table[0].strip().split("|")
+        data_rows = table[2].strip().split("\n")
+        header = [h.strip() for h in header if h.strip()]
+        data = [
+            [cell.strip() for cell in row.split("|") if cell.strip()]
+            for row in data_rows
+        ]
+        column_counts = {}
+        new_header = []
+        for col in header:
+            if col in column_counts:
+                column_counts[col] += 1
+                new_col_name = f"{col}_{column_counts}"
+            else:
+                column_counts[col] = 0
+                new_col_name = col
+            new_header.append(new_col_name)
+        df = pd.DataFrame(data, columns=new_header)
+        dataframes.append(df)
+
+    return dataframes
 
 
 def main():
@@ -68,6 +102,12 @@ def main():
             """
             result = analyze_image(prompt, base64_image, client)
             st.write(result)
+
+            # 마크다운 - 데이터프레임 변환 함수 호출 및 결과 출력
+            dataframes = markdown_to_dataframes(result)
+            st.write("## 데이터 다운로드")
+            for df in dataframes:
+                st.dataframe(df)
 
 
 if __name__ == "__main__":
